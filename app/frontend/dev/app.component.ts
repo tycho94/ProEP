1bf4efc1db7d41e0867b244fb2934864c7e54e45
ivegotasthma <ivegotasthma@openmailbox.org>
2016-06-03 14:14:17 +0200

    refactoring of the whole frontend p2

--- app/frontend/dev/app.component.ts
+++ app/frontend/dev/app.component.ts
@@ -1,11 +1,8 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-    selector: 'hello-world',
-    template: `
-        <h1>Angular 2 Boilerplate</h1>
-        <p>Hello World!</p>
-    `,
+    selector: 'app',
+    templateUrl: '',
 })
 
 export class AppComponent {
--- app/frontend/dev/bootstrap.ts
+++ app/frontend/dev/bootstrap.ts
@@ -1,6 +1,1 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
-import { HTTP_PROVIDERS } from "@angular/http";
-import { SignInComponent } from "./sign-in/sign-in.component";
-
-
-bootstrap(SignInComponent, [HTTP_PROVIDERS]);
